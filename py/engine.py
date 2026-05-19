f89d553a4ea60a6c4c8617caa3596ffba35e3d2e
andkom <akomissopoulos@gmail.com>
2026-05-19 04:39:43 -0400

    add op tracking

--- py/engine.py
+++ py/engine.py
@@ -1,19 +1,20 @@
 class Value:
-    def __init__(self, data, parents=()):
+    def __init__(self, data, parents=(), op=''):
         self.data = data
         self.parents = set(parents)
+        self.op = op
 
     def __repr__(self):
         return f'Value(data={self.data})'
 
     def __add__(self, other):
-        child = Value(self.data + other.data, (self, other))
+        child = Value(self.data + other.data, (self, other), op='+')
         return child
 
     def __mul__(self, other):
-        child = Value(self.data * other.data, (self, other))
+        child = Value(self.data * other.data, (self, other), op='*')
         return child
 
     def __div__(self, other):
-        child = Value(self.data / other.data, (self, other))
+        child = Value(self.data / other.data, (self, other), op='/')
         return child
