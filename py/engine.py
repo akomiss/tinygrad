ed912b4bed97b8dd40fdca819371ec061abada59
andkom <akomissopoulos@gmail.com>
2026-05-19 04:37:47 -0400

    add parent tracking

--- py/engine.py
+++ py/engine.py
@@ -1,15 +1,19 @@
 class Value:
-    def __init__(self, data):
-        self.data = data 
+    def __init__(self, data, parents=()):
+        self.data = data
+        self.parents = set(parents)
 
     def __repr__(self):
         return f'Value(data={self.data})'
 
     def __add__(self, other):
-        return Value(self.data + other.data)
-    
+        child = Value(self.data + other.data, (self, other))
+        return child
+
     def __mul__(self, other):
-        return Value(self.data * other.data)
+        child = Value(self.data * other.data, (self, other))
+        return child
 
     def __div__(self, other):
-        return Value(self.data * other.data)
+        child = Value(self.data / other.data, (self, other))
+        return child
