ff951dd2dd7c1175bff4bd65be429b289c71993a
andkom <akomissopoulos@gmail.com>
2026-05-20 11:50:33 -0400

    implement more derivs, begin changing tanh

--- py/engine.py
+++ py/engine.py
@@ -19,9 +19,19 @@ class Value:
         child = Value(self.data * other.data, (self, other), op='*')
         return child
 
+    def __sub__(self, other):
+        child = Value(self.data - other.data, (self, other), op='-')
+        return child
+
+    def __pow__(self, power):
+        child = Value(self.data ** power.data, (self, other), op='**')
+        return child
+
     def tanh(self):
         n = self.data
         val = (math.exp(2*n) - 1)/(math.exp(2*n) + 1)
+        Value(math.E)
+
         return Value(data=val, parents=(self, ), op='tanh', label='tanh()')
 
     def __div__(self, other):
@@ -42,9 +52,19 @@ class Value:
             if n.op == '+':
                 p1.grad += n.grad
                 p2.grad += n.grad
+            elif n.op == '-':
+                p1.grad -= n.grad
+                p2.grad -= n.grad
+            elif n.op == '/': # p1/p2 == p1 * (1/p2). incr p1 by 1 => 1/p2. incr p2 by 1 -> decrease by (p1/p2)
+                p1.grad += (1/p2.data) * n.grad
+                p2.grad += (-p1.data/(p2.data ** 2)) * n.grad
             elif n.op == '*':
                 p1.grad += p2.data * n.grad
                 p2.grad += p1.data * n.grad
+            elif n.op == '**':
+                p1.grad += (p2.data * (p1.data ** (p2.data - 1))) * n.grad
+                p2.grad += n.data * math.log(p1.data) * n.grad
+
             else:
                 raise RuntimeError('unknown operand', n.op)
 
